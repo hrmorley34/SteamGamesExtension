2feeacbac219824f73d1d9a9b3ac6509c9a30106
Henry Morley <henry@morley.org.uk>
2025-07-09 23:14:36 +0100

    Add context items to games

--- SteamGamesExtension/SteamGameListItem.cs
+++ SteamGamesExtension/SteamGameListItem.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VDFParser.Models;
+using Windows.System;
 
 namespace SteamGamesExtension;
 
@@ -15,7 +16,7 @@ public sealed partial class SteamGameListItem : ListItem
     public UInt32 AppId { get; init; }
     public int? LastPlayedTime { get; set; }
 
-    public SteamGameListItem(string title, IconInfo icon, UInt32 appId)
+    public SteamGameListItem(string title, IconInfo icon, UInt32 appId, IContextItem[]? moreCommands = null)
         : base()
     {
         Title = title;
@@ -26,6 +27,28 @@ public sealed partial class SteamGameListItem : ListItem
             Icon = icon,
             Result = CommandResult.Hide()
         };
+        MoreCommands = [
+            new CommandContextItem(
+                new OpenUrlCommand($"steam://open/games/details/{AppId}")
+                {
+                    Name = "Show in Library",
+                    Icon = new("\ue8f1"),  // Segoe Fluent 'Library'
+                    Result = CommandResult.Hide()
+                }
+            ),
+            new CommandContextItem(
+                new CopyTextCommand($"{AppId}")
+                {
+                    Name = "Copy App ID",
+                    Icon = new("\ue8c8"),  // Segoe Fluent 'Copy'
+                    Result = CommandResult.Hide()
+                }
+            )
+            {
+                RequestedShortcut = KeyChordHelpers.FromModifiers(ctrl: true, vkey: VirtualKey.C)
+            },
+            ..moreCommands ?? []
+        ];
     }
 
     public static SteamGameListItem FromManifest(AppManifestACF appManifest)
@@ -34,10 +57,20 @@ public sealed partial class SteamGameListItem : ListItem
         return new SteamGameListItem(
             appState.name,
             new("\ue7fc"),  // Segoe Fluent 'Game'
-            appState.appid
+            appState.appid,
+            [
+                new CommandContextItem(
+                    new OpenUrlCommand($"steam://store/{appState.appid}")
+                    {
+                        Name = "Show in Store",
+                        Icon = new("\ue7bf"),  // Segoe Fluent 'Shopping Cart'
+                        Result = CommandResult.Hide()
+                    }
+                )
+            ]
         )
         {
-            Subtitle = $"ID: {appState.appid}; {appState.installdir}"
+            Subtitle = $"ID: {appState.appid}; {appState.installdir}",
         };
     }
 
